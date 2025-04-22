3d0c015f280b90728406ebd1f9c124b003f1c750
Lorcania11 <169111954+Lorcania11@users.noreply.github.com>
2025-04-22 14:19:33 +0000

    Enhance configuration: Add extra settings in app.json, update Babel presets in babel.config.js, and modify package.json dependencies for improved compatibility and performance.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["@babel/preset-env"],
+    presets: [
+      "babel-preset-expo",
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript"
+    ],
     plugins: [
-      'react-native-reanimated/plugin',
+      "react-native-reanimated/plugin",
     ],
   };
 };
