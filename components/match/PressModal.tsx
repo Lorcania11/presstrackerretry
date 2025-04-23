8467ca28d65ef1f7439c3cc67e5758d9555a76c4
Lorcania11 <169111954+Lorcania11@users.noreply.github.com>
2025-04-23 18:45:10 +0000

    Tailwind CSS implementation instead of Style Sheet

--- components/match/PressModal.tsx
+++ components/match/PressModal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import { Teams } from "../types";
+import { Teams } from "./types";
 
 interface PressModalProps {
   visible: boolean;
