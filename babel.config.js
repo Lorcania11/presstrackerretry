2240cb8f9a8aeddeaa7d7c3e17d6de5aeb72aa90
Lorcania11 <169111954+Lorcania11@users.noreply.github.com>
2025-04-24 00:07:40 +0000

    y

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      require.resolve('react-native-reanimated/plugin'),
-      require.resolve('nativewind/babel')
+      'react-native-reanimated/plugin',
+      'nativewind/babel'
     ]
   };
 };
