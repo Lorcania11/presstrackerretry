0659b3c26d1cc152a77ebacf763411c4088adf7a
Lorcania11 <169111954+Lorcania11@users.noreply.github.com>
2025-04-26 17:33:55 +0000

    Remove unused plugins from tsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'react-native-reanimated/plugin',
-      'nativewind/babel'
+      'nativewind/babel',
+      'expo-router/babel'
     ]
   };
 };
