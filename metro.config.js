6de920b9a3eab04e9d03e38230db14e706b6ce4e
Lorcania11 <169111954+Lorcania11@users.noreply.github.com>
2025-05-03 07:03:09 +0000

    chore: add @react-native/metro-config dependency to package.json

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config');
 
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 const config = getDefaultConfig(__dirname);
-
-// Add this to handle Expo Router properly
-config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs'];
 
 module.exports = config;
